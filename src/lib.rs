cfc144ae18bfa8e2b943827156219c574fc2b199
Diego Garcia <wft2000@gmail.com>
2018-08-19 22:30:54 +0200

    login to server (hardcoded credentials)

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,8 @@ use std::io::prelude::*;
 use std::net::TcpStream;
 use std::error::Error;
 
+use std::io::{BufReader, BufWriter};
+
 pub struct Config {
     pub host: String,
 }
@@ -21,15 +23,18 @@ impl Config {
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 
-    let mut stream = TcpStream::connect(config.host)?;
+    
+    let stream = TcpStream::connect(config.host)?;
+    let mut reader = BufReader::new(&stream);
+    let mut writer = BufWriter::new(&stream);
 
-    let mut buffer = [0; 128];
-    stream.read(&mut buffer)?;
-    println!("{}", String::from_utf8(buffer.to_vec()).unwrap());
+    read_server_response(&mut reader)?;
+
+    login_to_server(&mut reader, &mut writer, "anonymous", "wft2000@gmail.com")?;
 
     loop {
         print!("rftp>");
-        io::stdout().flush().expect("Problem writing output!");
+        io::stdout().flush()?;
 
         let mut command = String::new();
         io::stdin().read_line(&mut command).expect("Failed to read line");
@@ -40,5 +45,39 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
         }
     }
 
+    Ok(())
+}
+
+fn login_to_server(
+    reader: &mut BufReader<&TcpStream>, 
+    writer: &mut BufWriter<&TcpStream>,
+    username: &str,
+    password: &str) -> Result<(), std::io::Error> {
+
+    let mut user_command: String = "USER ".to_owned();
+    user_command.push_str(username);
+    user_command.push('\n');
+    send_command_to_server(writer, user_command.as_bytes())?;  // enter login
+    read_server_response(reader)?;
+
+    let mut pass_command: String = "PASS ".to_owned();
+    pass_command.push_str(password);
+    pass_command.push('\n');
+    send_command_to_server(writer, pass_command.as_bytes())?;  // enter password
+    read_server_response(reader)?;
+
+    Ok(())
+}
+
+fn read_server_response(reader: &mut BufReader<&TcpStream>) -> Result<(), std::io::Error> {
+    let mut buffer = String::new();
+    reader.read_line(&mut buffer)?;
+    println!("{}", buffer.trim());
+    Ok(())
+}
+
+fn send_command_to_server(writer: &mut BufWriter<&TcpStream>, command: &[u8]) -> Result<(), std::io::Error> {
+    writer.write(command)?;
+    writer.flush()?;
     Ok(())
 }
